b07950e16b3aba097406511b8578f2816c9cc615
blakeablitt <blake@b-mak.com>
2023-03-26 19:39:41 -0700

    Timing Fixed

--- app/public/scripts/timing-script.php
+++ app/public/scripts/timing-script.php
@@ -1,25 +1,26 @@
-<?php session_start();
+<?php 
+session_start();
 $createdAt =  $_GET["timestamp"];
-$timezone = date_default_timezone_set('UTC');
-$createdAt = strtotime($createdAt);
 $currentTime = time();
+$timeSincePost = $currentTime - $createdAt;
 
-$timeSincePost = $currentTime - $createdAt-43200;
-
-
-if($timeSincePost < 60){
-    $timeReturned = 'Less than a minute ago';
-}elseif($timeSincePost < 3600){
-    $timeReturned = floor($timeSincePost/60).' minutes ago';
-}elseif($timeSincePost < 8600 && floor($timeSincePost/3600) ==1) {
-    $timeReturned = floor($timeSincePost/3600).' hour ago';
-}elseif($timeSincePost < 8600 && floor($timeSincePost/3600) >1) {
-    $timeReturned = floor($timeSincePost/3600).' hours ago';
-}elseif(intval(floor($timeSincePost/86400)) == 1) {
-    $timeReturned = floor($timeSincePost/86400).' day ago';
-}elseif(floor($timeSincePost/86400) > 1){
-    $timeReturned = floor($timeSincePost/86400).' days ago';
+if ($timeSincePost < 60) {
+    $timeReturned = 'Posted just now';
+} elseif ($timeSincePost < 3600) {
+    $minutes = floor($timeSincePost/60);
+    $timeReturned = $minutes == 1 ? '1 minute ago' : $minutes.' minutes ago';
+} elseif ($timeSincePost < 86400) {
+    $hours = floor($timeSincePost/3600);
+    $timeReturned = $hours == 1 ? '1 hour ago' : $hours.' hours ago';
+} else {
+    $days = floor($timeSincePost/86400);
+    $timeReturned = $days == 1 ? '1 day ago' : $days.' days ago';
 }
 
 echo $timeReturned;
 ?>
+
+
+
+
+
